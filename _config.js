0ba5b498cfaca264cd101a726727e1c3e43270e0
Sporule <hao@sporule.com>
2019-11-02 11:15:42 +0000

    updated css

--- _config.js
+++ _config.js
@@ -4,7 +4,7 @@ const Config = {
     url:"https://www.eatsomemore.com",
     description: "三十六计至上决，万里之行吃什么。",
     keywords:"food, travel",
-    logo:"https://i.imgur.com/MrRLOC4.png",
+    logo:"/images/logo.png",
     disqusShortname:"eatsomemore",
     postPerPage:24,
     googleAnaltics:"UA-114032457-2",
